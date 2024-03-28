da83d3130026f1525dc82aaddce1227f524d796f
stevehunts <stevenvphiri95@gmail.com>
2024-03-28 03:05:48 -0700

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]  # Slices the first 3 characters
+word_last_2 = word[-2:]  # Slices the last 2 characters
+middle_word = word[1:-1]  # Slices the string except for the first and last characters
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
