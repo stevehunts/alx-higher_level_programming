09cf555447657e7af843f85fb0199d1c8a8ab005
stevehunts <stevenvphiri95@gmail.com>
2024-04-01 11:57:23 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
+
+guillaume@ubuntu:~/0x03$ ./10-main.py
+0 is divisible by 2
+1 is not divisible by 2
+2 is divisible by 2
+3 is not divisible by 2
+4 is divisible by 2
+5 is not divisible by 2
+6 is divisible by 2
