bd58973cad8bf51cafa339bb2d4920789233c8df
stevehunts <stevenvphiri95@gmail.com>
2024-04-07 15:18:49 -0700

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-main.py
+++ 0x05-python-exceptions/6-main.py
@@ -1,4 +1,7 @@
-# This is the content of 6-raise_exception_msg.py
-def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
-    raise NameError(message)
+#!/usr/bin/python3
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
