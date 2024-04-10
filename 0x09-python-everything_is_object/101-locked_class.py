5efcc14c83a342c70cad587c11b1562dd7513f0a
stevehunts <stevenvphiri95@gmail.com>
2024-04-10 15:18:15 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """Defines a locked class."""
 
-
 class LockedClass:
     """
-    Prevent the user from instantiating new LockedClass attributes for anything but attributes called 'first_name'.
+    Prevent the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called 'first_name'.
     """
-
     __slots__ = ["first_name"]
+
+# Below code is for testing the functionality.
+# It's not part of the class definition.
+if __name__ == "__main__":
+    lc = LockedClass()
+    lc.first_name = "John"
+    try:
+        lc.last_name = "Snow"
+    except AttributeError as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
