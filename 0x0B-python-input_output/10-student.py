7268560c372dc7dca3c117a3367d987857e8b782
stevehunts <stevenvphiri95@gmail.com>
2024-04-17 15:34:33 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Student Class."""
-
 class Student:
     """Defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initialize a new Student with given attributes.
 
         Args:
             first_name (str): The first name of the student.
@@ -17,15 +15,30 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """Retrieve a dictionary representation of a Student instance.
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+        If attrs is a list of strings, only attributes with names in this list
+        are included. Otherwise, all attributes are included.
 
         Args:
-            attrs (list): (Optional) The attributes to represent.
+            attrs (list, optional): The attributes to include in the dictionary.
+
+        Returns:
+            dict: A dictionary representation of the student.
         """
-        
-        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict___
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+# Example usage
+if __name__ == "__main__":
+    student_1 = Student("John", "Doe", 23)
+    student_2 = Student("Bob", "Dylan", 27)
+
+    j_student_1 = student_1.to_json()
+    j_student_2 = student_2.to_json(['first_name', 'age'])
+    j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+    print(j_student_1)
+    print(j_student_2)
+    print(j_student_3)
