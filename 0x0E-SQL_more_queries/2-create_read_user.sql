81fa1c9bd76178d1f152ea5034b72df8c700e7f0
stevehunts <stevenvphiri95@gmail.com>
2024-05-23 16:44:24 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,8 @@
--- 1-create_user.sql
--- Creates the user user_0d_1 with all privileges if it does not exist
+-- Check and create the user user_0d_1 with all privileges if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grants all privileges on the entire server to user_0d_1
+-- Grant all privileges on the entire server to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Applies the changes
+-- Apply the changes
 FLUSH PRIVILEGES;
